2e7f5ad1b5493588bb386c9cfa381e7535371e88
Mario <mariobernal0980@gmail.com>
2026-08-30 19:32:54 +0200

    Connect register with backend

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { createPlayer } from "../services/playerService";
 
 function RegisterPage() {
 
-    const handleRegister = (event) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleRegister = async (event) => {
         event.preventDefault();
 
+        if (password !== confirmPassword) {
+            alert("The passwords don't match");
+            return;
+        }
+        
+        await createPlayer(username, password);
+        navigate("/");
     };
+
     return (
         <div>
             <h1>Tic Tac Toe</h1>
@@ -21,6 +37,8 @@ function RegisterPage() {
                         type="text"
                         id="username"
                         name="username"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
                         required
                     />
                 </div>
@@ -34,6 +52,8 @@ function RegisterPage() {
                         type="password"
                         id="password"
                         name="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
                         required
                     />
                 </div>
@@ -47,6 +67,8 @@ function RegisterPage() {
                         type="password"
                         id="confirmPassword"
                         name="confirmPassword"
+                        value={confirmPassword}
+                        onChange={(event) => setConfirmPassword(event.target.value)}
                         required
                     />
                 </div>
